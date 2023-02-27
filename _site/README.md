33da6dfba418edd43e4f299c22deee385f2ff988
Stanislav <stanislavkrstev8@gmail.com>
2023-02-27 08:33:14 +0200

    Adding cities

---
+++ _site/README.md
@@ -0,0 +1,28 @@
+# Bulgarian_Universities
+
+A comprehensive resource for students interested in pursuing higher education in Bulgaria. This website provides information on the top universities in Bulgaria, their respective majors and a brief overview of each.
+
+# Features
+-Detailed information on the top universities in Bulgaria
+-Information on the various majors offered at each university
+-Overview of each university and its facilities
+-Average point calculator for each major, based on the admission requirements of each university
+-User-friendly interface and responsive design for easy navigation on any device
+# Technologies
+This website is built using the following technologies:
+
+-HTML
+-CSS
+-JavaScript
+-JQuery
+-Databases
+# Usage
+Clone or download the repository
+Open index.html in your browser
+Browse the various universities and their respective majors
+View information on each university and its facilities
+Use the average point calculator to determine your eligibility for admission to a specific major
+Contributing
+If you wish to contribute to this project, please fork the repository and make the desired changes to your fork. Once you are satisfied with your changes, please open a pull request and I will review it promptly.
+# OR 
+Check: https://stank1.github.io/Bulgarian_Universities/
---
+++ _site/assets/js/main.js
@@ -0,0 +1,429 @@
+
+(function() {
+  "use strict";
+
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
+
+
+/**
+   * Clients Slider
+   */
+new Swiper('.clients-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 80
+    },
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 120
+    }
+  }
+});
+
+
+window.addEventListener('load', () => {
+  let galleryContainer = select('.gallery-container');
+  if (galleryContainer) {
+    let galleryIsotope = new Isotope(galleryContainer, {
+      itemSelector: '.gallery-item'
+    });
+
+    
+  }
+
+});
+
+
+
+/**
+ * Initiate gallery lightbox 
+ */
+const galleryLightbox = GLightbox({
+  selector: '.gallery-lightbox'
+});
+
+/**
+ * gallery details slider
+ */
+new Swiper('.gallery-details-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+
+
+/**
+   * Testimonials slider
+   */
+new Swiper('.testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener('load', () => {
+  AOS.init({
+    duration: 1000,
+    easing: "ease-in-out",
+    once: true,
+    mirror: false
+  });
+});
+
+/**
+ * Initiate Pure Counter 
+ */
+new PureCounter();
+
+
+
+
+function calculate() {
+  // Get the input values
+  var a = document.getElementById("inputA").value;
+  var b = document.getElementById("inputB").value;
+  var c = document.getElementById("inputC").value;
+  var d = document.getElementById("inputD").value;
+  var e = document.getElementById("inputE").value;
+  var f = document.getElementById("inputF").value;
+  
+
+  // Convert the input values to numbers
+  a = Number(a);
+  b = Number(b);
+  c = Number(c);
+  d = Number(d);
+  e = Number(e);
+  f = Number(f);
+
+  // Check if any of the input values are outside the allowed range
+  var error = false;
+  if (a < 2 || a > 6 || b < 2 || b > 6 || c < 2 || c > 6 || d < 2 || d > 6 || e < 2 || e > 6 || f < 2 || f > 6) {
+    error = true;
+  }
+
+  // If there is an error, display the error message and return
+  if (error) {
+    document.getElementById("error").style.display = "block";
+    return;
+  }
+
+  // Get the selected
+  formula
+    var formula = document.getElementById("formulaSelect").value;
+
+    // Initialize the result variable
+    var result;
+
+    // Choose the formula based on the selected option
+    if (formula === "addition") {
+      result = a + b + c + d + e + f;
+    } else if (formula === "subtraction") {
+      result = a - b - c - d - e - f;
+    } else if (formula === "multiplication") {
+      result = a * b * c * d * e * f;
+    } else if (formula === "delenie") {
+      result = a / b / c / d / e / f;
+    }
+
+    // Set the result in the output element and hide the error message
+    document.getElementById("output").value = result;
+    document.getElementById("error").style.display = "none";
+  }
+
+
+
+  function search() {
+    // Get the search query from the input field
+    const query = document.getElementById('search-input').value.toLowerCase();
+  
+    // Get all the university elements
+    const universities = document.querySelectorAll('.university');
+  
+    // Loop through the universities and hide those that don't match the search query
+    universities.forEach(university => {
+      if (university.textContent.toLowerCase().includes(query)) {
+        university.style.display = 'block';
+      } else {
+        university.style.display = 'none';
+      }
+    });
+  }
+
+
+// Callculator drop boxes
+  function updateTextBox2() {
+    var textbox1 = document.getElementById("textbox1");
+    var textbox2 = document.getElementById("textbox2");
+
+    // Clear the options of the second drop-down menu
+    textbox2.innerHTML = "";
+
+    // Get the selected option of the first drop-down menu
+    var selectedOption = textbox1.options[textbox1.selectedIndex].text;
+
+    // Add the appropriate options to the second drop-down menu based on the selection in the first drop-down menu
+    if (selectedOption == "Софийски университет \"Св. Климент Охридски\"") {
+      textbox2.innerHTML += "<option>Философия</option><option>Психология</option><option>Социология</option><option>Политология</option><option>Публична администрация</option><option>Културология</option><option>Библиотечно-информационни науки</option><option>Европеистика</option><option>Публични информационни системи</option><option>Философия (на английски език)</option>";
+    } else if (selectedOption == "Медицински университет София") {
+      textbox2.innerHTML += "<option>Option 4</option><option>Option 5</option><option>Option 6</option>";
+    } else if (selectedOption == "Технически университет София") {
+      textbox2.innerHTML += "<option>Option 7</option><option>Option 8</option><option>Option 9</option>";
+    }
+  }
+
+  function showResult() {
+    var textbox2 = document.getElementById("textbox2");
+    var resultLabel = document.getElementById("resultLabel");
+
+    // Get the selected option of the second drop-down menu
+    var selectedOption = textbox2.options[textbox2.selectedIndex].text;
+
+    // Update
+    resultLabel.innerHTML = "Result: " + selectedOption;
+  }
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
